58f60e9a064db3fee2e29b5abb34d9a43b7397a4
Jinho <40512164+jinh0@users.noreply.github.com>
2020-07-20 14:47:23 +0900

    Created order models and basic display of orders.

--- food/models.py
+++ food/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=50)
@@ -32,5 +33,27 @@ class MenuItem(models.Model):
 
     category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='menu', null=True)
 
+    total_count = models.IntegerField(default=0)
+
     def __str__(self):
-        return '%s - %s' % (self.name, self.price)
+        return f'{self.name} - {self.price}'
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='orders')
+    restaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='orders')
+
+    status = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'{self.user} -> {self.restaurant}, {self.status}'
+
+
+class OrderItem(models.Model):
+    orderList = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='items')
+    menuItem = models.ForeignKey(MenuItem, on_delete=models.CASCADE, related_name='orders')
+
+    price = models.IntegerField(default=0)
+    count = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.menuItem} {self.count}'
--- food/urls.py
+++ food/urls.py
@@ -6,8 +6,12 @@ urlpatterns = [
     path("restaurant/<int:restaurant_id>", views.restaurant, name='restaurant'),
     path("cuisine/<currentCuisine>", views.cuisine, name='cuisine'),
     path("cuisines", views.cuisines, name='cuisines'),
+
+    path("orders", views.orders, name='orders'),
+
     path("login", views.login_view, name='login'),
     path("logout", views.logout_view, name='logout'),
     path("register", views.register, name='register'),
+
     path("about", views.about, name='about'),
 ]
