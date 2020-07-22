ebb7a37ae50bc15bf3eb9d1f00b5853d76050344
danielkim428 <47778502+danielkim428@users.noreply.github.com>
2020-07-22 18:34:18 +0900

    Fixed Current Orders witout Forloops

--- food/templatetags/food_extra.py
+++ food/templatetags/food_extra.py
@@ -5,3 +5,13 @@ register = template.Library()
 @register.filter
 def animate(value, arg):
     return arg + value * 0.2
+
+@register.filter
+def matchingID(items, menuID):
+    try:
+        item = items.filter(menuItem=menuID).first()
+        result = item.count
+    except:
+        result = 0
+        
+    return result
