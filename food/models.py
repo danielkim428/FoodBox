20c8bc52c4a4d20a1c4195ce40f86a94043df93b
Jinho <40512164+jinh0@users.noreply.github.com>
2020-07-29 19:40:09 +0900

    Add basic phone number functionality.

--- food/models.py
+++ food/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 class Cuisine(models.Model):
     name = models.CharField(max_length=50)
@@ -64,3 +66,29 @@ class OrderItem(models.Model):
 
     def __str__(self):
         return f'{self.menuItem} {self.count}'
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=64, blank=True)
+    phoneNumber = models.CharField(max_length=64, blank=True)
+
+    ownedRestaurant = models.ForeignKey(Restaurant, on_delete=models.CASCADE, related_name='owners', null=True)
+
+    birthDate = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return f'{self.user}, {self.location}'
+
+# Listeners that save the profile when the user is saved
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    try:
+        instance.profile.save()
+    except:
+        Profile.objects.create(user=instance)
+        instance.profile.save()
