06af5f87dd335a66baacd0b6cd14441a5e291170
Jinho <40512164+jinh0@users.noreply.github.com>
2020-07-29 19:40:33 +0900

    Add restaurant orders for restaurant owners.

--- food/views.py
+++ food/views.py
@@ -15,8 +15,12 @@ def index(request):
     context = {
         "restaurants": Restaurant.objects.all(),
         "cuisine": Cuisine.objects.all(),
-        "currentTime": datetime.datetime.now()
+        "currentTime": datetime.datetime.now(),
     }
+
+    # For restaurant owners
+    if request.user.is_authenticated:
+        context["ownedRestaurant"] = request.user.profile.ownedRestaurant
 
     return render(request, "food/index.html", context)
 
@@ -25,10 +29,53 @@ def orders(request):
         return HttpResponseRedirect(reverse('login'))
     else:
         context = {
-            "orders": request.user.orders.all()
+            "choosingOrders": request.user.orders.annotate(num_items=Count('items')).filter(status=0).exclude(num_items=0),
+            "pendingOrders": request.user.orders.filter(status=1),
+            "deliveringOrders": request.user.orders.filter(status=2),
+            "deliveredOrders": request.user.orders.filter(status=3),
+            "phoneNumber": request.user.profile.phoneNumber
         }
 
         return render(request, "food/orders.html", context)
+
+def restaurantOrders(request, restaurantId):
+    restaurant = Restaurant.objects.get(id=restaurantId)
+
+    if request.user.profile.ownedRestaurant == restaurant:
+        if request.method == 'POST':
+            action = request.POST['action']
+            orderId = request.POST['order-id']
+            order = Order.objects.get(id=orderId)
+
+            if action == 'confirm':
+                # Pending --> Delivering status
+                order.status = 2
+
+                # TODO WhatsApp bot to customer
+            elif action == 'delivered':
+                # Delivering --> Delivered status
+                order.status = 3
+
+                # TODO WhatsApp bot to customer
+            elif action == 'reject':
+                # Pending --> Choosing status
+                order.status = 0
+
+                # TODO WhatsApp bot to customer
+                
+            order.save()
+            return HttpResponseRedirect(reverse('restaurantOrders', args=[restaurantId]))
+        else:
+            context = {
+                "restaurant": restaurant,
+                "pendingOrders": restaurant.orders.filter(status=1),
+                "deliveringOrders": restaurant.orders.filter(status=2),
+                "deliveredOrders": restaurant.orders.filter(status=3),
+            }
+
+            return render(request, "food/restaurantOrders.html", context)
+    else:
+        return HttpResponseRedirect(reverse('index'))
 
 def about(request):
     return render(request, "food/about.html")
@@ -66,7 +113,8 @@ def restaurant(request, restaurantId):
     context = {
         "restaurant": restaurant,
         "categories": restaurant.categories.all(),
-        "currentTime": datetime.datetime.now()
+        "currentTime": datetime.datetime.now(),
+        "phoneNumber": request.user.profile.phoneNumber
         # "menuItems": MenuItem.objects.filter(category__restaurant=restaurant)
     }
 
@@ -74,8 +122,9 @@ def restaurant(request, restaurantId):
         if request.method == 'POST':
             orderId = request.POST['orderId']
             order = Order.objects.get(id=orderId)
-            # order.status = 1
-            order.save()
+
+            if request.user.profile.phoneNumber == '':
+                return HttpResponseRedirect(reverse('phoneNumber'))
 
             return HttpResponseRedirect(reverse('address', args=[restaurantId]))
         else:
@@ -156,6 +205,25 @@ def login_view(request):
     else:
         return render(request, 'food/login.html')
 
+# People who register are redirected to this view to confirm their phone number
+# TODO WhatsApp bot integration
+def phoneNumber(request):
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            phoneNumber = request.POST['phoneNumber']
+            print(phoneNumber)
+
+            # TODO
+            user = request.user
+            user.profile.phoneNumber = phoneNumber
+            user.save()
+
+            return HttpResponseRedirect(reverse('orders'))
+        else:
+            return render(request, 'food/phoneNumber.html')
+    else:
+        return HttpResponseRedirect(reverse('login'))
+
 def logout_view(request):
     logout(request)
     return render(request, "food/login.html", {"message": "Logged out."})
@@ -165,14 +233,13 @@ def register(request):
         return HttpResponseRedirect(reverse('index'))
 
     if request.method == 'POST':
-        email = request.POST['email']
         username = request.POST['username']
         password = request.POST['password']
         first_name = request.POST['firstname']
         last_name = request.POST['lastname']
 
         try:
-            new_user = User.objects.create_user(username, email, password, first_name=first_name, last_name=last_name)
+            new_user = User.objects.create_user(username, password=password, first_name=first_name, last_name=last_name)
             new_user.save()
 
         except IntegrityError:
@@ -181,6 +248,5 @@ def register(request):
             return render(request, 'food/register.html', {"message": "Invalid credentials."})
 
         return HttpResponseRedirect(reverse('login'))
-
     else:
         return render(request, 'food/register.html')
